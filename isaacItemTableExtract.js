c496d39325cb372825d6c71f26c9bf481bf6013f
Kai Arnold <cdn_kai@yahoo.com>
2016-01-01 11:06:01 -0800

    migrate isaac stuff to its own folder

---
+++ isaacItemTableExtract.js
@@ -0,0 +1,67 @@
+// Current workflow:
+//
+// 1) open the isaac rebirth wiki page to scrape
+// 2) paste this file directly in the dev console
+// 3) run scrapeTable() and scrapeTableSublementalTemplate, save them directly to files
+// 3.5) there may be more than one table of items to scrape per page, so pass in different table indices as appropriate
+// 4) save the files where the combiner script can find them
+
+var g_descriptionCol = 2; // changes for cards and runes
+function scrapeTable(tableIndex)
+{
+	return JSON.stringify(extractToJSON(getTable(tableIndex), rowImageAndThumbnailScrape), null, '\t');
+}
+function scrapeTableTemplate(tableIndex)
+{
+	return JSON.stringify(extractToJSON(getTable(tableIndex), rowMetadataTemplateScrape), null, '\t');
+}
+function extractToJSON(table, rowScraper)
+{
+	var retval = {};
+
+	var nRows = table.rows.length;
+	for (var r = 1; r < nRows; ++r)
+	{
+		var row = table.rows[r];
+		var nCols = row.cells.length;
+
+		var name = row.cells[0].textContent;
+		retval[name] = rowScraper(row);
+	}
+	return retval;
+}
+function rowImageAndThumbnailScrape(row)
+{
+	var entry = {};
+
+	var anchor = getChildTag(row.cells[0], "a");
+	if (anchor)
+	{
+		entry.wikiPage = anchor.href;
+	}
+
+	var img = getChildTag(row, "img");
+	entry.thumbnail = img.src;
+
+	var description = row.cells[g_descriptionCol];
+	entry.descriptionHTML = description.innerHTML;
+	return entry;
+}
+function rowMetadataTemplateScrape(row)
+{
+	var entry = {};
+	entry.itemClass = "";
+	entry.itemType = "";
+	entry.itemColor = "";
+	entry.itemTags = "";
+	return entry;
+}
+function getTable(index)
+{
+	return document.body.getElementsByTagName("table")[index];
+}
+function getChildTag(el, tagName)
+{
+	var elements = el.getElementsByTagName(tagName);
+	return elements && elements.length && elements[0];
+}
---
+++ thesearchingofisaac.js
@@ -0,0 +1,126 @@
+var g_testData =
+{
+	"items":
+	[
+		{ "name": "9 volt", "tags": "battery cube duracell"},
+		{ "name": "Abaddon", "tags": "penta star red"},
+		{ "name": "Abel", "tags": "familiar face dead baby gray"},
+		{ "name": "Anemic", "tags": "tear red"},
+		{ "name": "Aquarius", "tags": "zodiac blue waves"},
+		{ "name": "Aries", "tags": "zodiac blue ram"},
+		{ "name": "Ball Of Bandages", "tags": "familiar orbital round pink"}
+	],
+	"aliases":
+	[
+		"battery charge",
+		"syringe needle",
+		"black dark gray grey",
+		"fly flies",
+		"zodiac horoscope horroscope",
+		"round circle ball sphere"
+	]
+};
+
+var g_itemClasses = "activated passive card rune pill"
+
+var g_testData_V2 = 
+{
+	"items":
+	{
+		"The Bible" :
+		{
+			wikiPage: "...",
+			thumbnail: "...",
+			descriptionHTML: "...",
+
+			itemClass: "activated",
+			type: "book",
+			color: "brown",
+			tags: ""
+		},
+		"Big Fan":
+		{
+			itemClass: "passive",
+			type: "familiar",
+			color: "brown beige",
+			tags: "fly orbital"
+		}
+	};
+};
+
+
+function prepareData(data)
+{
+	// process the aliases first
+	var aliasLookup = {};
+	data.aliases.forEach(function(aliasString, i, array)
+	{
+		var aliases = aliasString.split(' ');
+		aliases.forEach(function(alias, i, array)
+		{
+			aliasLookup[alias] = aliasString;
+		});
+	});
+
+
+	//KAI: maybe include a "class" field.  When scoring matches, the name is weighted higher than class, then higher then tags
+	// add a by-name lookup
+	data.itemLookup = {};
+
+	// convert item.tags to a hash for faster lookups (KAI: is it really faster than just indexOf on .tags?)
+	data.items.forEach(function(item, i, array)
+	{
+		item.nameLowerCase = item.name.toLowerCase();
+		data.itemLookup[item.nameLowerCase] = item;
+
+		var aliasesToAppend = "";
+		var tags = item.tags.split(' ');
+		tags.forEach(function(tag, i, array)
+		{
+			if (aliasLookup[tag])
+			{
+				aliasesToAppend += " " + aliasLookup[tag];
+			}
+		});
+		item.tags += " " + aliasesToAppend;
+	});
+}
+
+prepareData(g_testData);
+
+function retrieveHits(data, searchText)
+{
+	// split search into multiple terms
+	var terms = searchText.toLowerCase().split(' ');  // KAI: should maybe regexp for whitespace instead
+
+	// scan the items, gather the hits
+	var hits = [];
+	var nTerms = terms.length;
+	data.items.forEach(function(item, i, array)
+	{
+		for (var i = 0; i < nTerms; ++i)
+		{
+			var term = terms[i];
+			if (item.nameLowerCase.indexOf(term) >= 0 || item.tags.indexOf(term) >= 0)
+			{
+				hits.push(item);
+				break;
+			}
+		}
+	});
+
+	return hits;
+}
+
+//KAI: searching rules:
+// 1) maybe handle plurals
+// 2) audit/normalize item.tags so that aliases don't appear? maybe not  
+function update()
+{
+	var terms = event.currentTarget.value;
+	if (terms.length > 0)
+	{
+		var hits = retrieveHits(g_testData, event.currentTarget.value);
+		console.log("hits " + hits.length);
+	}
+}
